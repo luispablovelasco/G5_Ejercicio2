b9d9822a17b6a9da4e56588c0e9f8a33cec38c67
luispablovelasco <pablo.velascoflores@gmail.com>
2021-10-26 20:59:24 -0600

    Creacion de la clase Alumno y interfaz visual del formulario LOGIN

---
+++ G5_Ejercicio2F/G5_Ejercicio2F/Alumno.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace G5_Ejercicio2F
+{
+    class Alumno
+    {
+
+        //Atributos
+        private string nombre;
+        private DateTime nacimiento;
+        private string correo;
+        private string responsable;
+        private string carnet;
+        
+        //Propiedades con validación
+        public string Nombre
+        {
+            get { return nombre; }
+
+            set
+            {
+                nombre = value;
+                if (nombre == "")
+                {
+                    throw new Exception("No tiene que haber espacios en blanco");
+                }
+
+                foreach (char letra in nombre)
+                {
+                    //Permitimos lo siguiente
+                    switch (letra)
+                    {
+
+                        case 'Á': continue;
+                        case 'É': continue;
+                        case 'Í': continue;
+                        case 'Ó': continue;
+                        case 'Ú': continue;
+
+                    }
+
+
+                }
+            }
+        }
+
+        public DateTime Nacimiento
+        {
+            get { return nacimiento; }
+            set
+            {
+                nacimiento = value;
+
+                if (nacimiento > System.DateTime.Now.Date)
+                {
+                    throw new Exception("Ese nacimiento aun no ha ocurrido");
+                }
+            }
+        }
+
+        public string Correo
+        {
+            get { return correo; }
+            set
+            {
+                correo = value;
+
+                string expresion = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9- ]+)*(.[a - z]{ 2,4})$";
+                if (Regex.IsMatch(correo, expresion))
+                {
+                    if (Regex.Replace(correo, expresion, string.Empty).Length == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        throw new Exception("Direccion de correo no valida");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Direccion de correo no valida");
+                }
+            }
+        }
+
+        public string Responsable
+        {
+            get { return responsable; }
+            set
+            {
+                responsable = value;
+                if (responsable == "")
+                {
+                    throw new Exception("No tiene que haber espacios en blanco");
+                }
+
+                foreach (char letra in responsable)
+                {
+                    //Permitimos lo siguiente
+                    switch (letra)
+                    {
+
+                        case 'Á': continue;
+                        case 'É': continue;
+                        case 'Í': continue;
+                        case 'Ó': continue;
+                        case 'Ú': continue;
+
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ G5_Ejercicio2F/G5_Ejercicio2F/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace G5_Ejercicio2F
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mskboxcarnet = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpnacimiento = new System.Windows.Forms.DateTimePicker();
+            this.txtcorreo = new System.Windows.Forms.TextBox();
+            this.txtrespon = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btningresar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mskboxcarnet
+            // 
+            this.mskboxcarnet.Location = new System.Drawing.Point(96, 75);
+            this.mskboxcarnet.Mask = "LL000000";
+            this.mskboxcarnet.Name = "mskboxcarnet";
+            this.mskboxcarnet.Size = new System.Drawing.Size(116, 20);
+            this.mskboxcarnet.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Carnet:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombres:";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(111, 121);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(261, 20);
+            this.txtnombre.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 14);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha de nacimiento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 14);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Correo electronico:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(43, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 14);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Responsables:";
+            // 
+            // dtpnacimiento
+            // 
+            this.dtpnacimiento.Location = new System.Drawing.Point(172, 171);
+            this.dtpnacimiento.Name = "dtpnacimiento";
+            this.dtpnacimiento.Size = new System.Drawing.Size(200, 20);
+            this.dtpnacimiento.TabIndex = 7;
+            // 
+            // txtcorreo
+            // 
+            this.txtcorreo.Location = new System.Drawing.Point(162, 224);
+            this.txtcorreo.Name = "txtcorreo";
+            this.txtcorreo.Size = new System.Drawing.Size(210, 20);
+            this.txtcorreo.TabIndex = 8;
+            // 
+            // txtrespon
+            // 
+            this.txtrespon.Location = new System.Drawing.Point(135, 270);
+            this.txtrespon.Name = "txtrespon";
+            this.txtrespon.Size = new System.Drawing.Size(237, 20);
+            this.txtrespon.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(192, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "LOGIN";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::G5_Ejercicio2F.Properties.Resources.unnamed;
+            this.pictureBox1.Location = new System.Drawing.Point(411, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(236, 268);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btningresar
+            // 
+            this.btningresar.BackColor = System.Drawing.Color.Gold;
+            this.btningresar.Font = new System.Drawing.Font("Lucida Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btningresar.ForeColor = System.Drawing.Color.Black;
+            this.btningresar.Location = new System.Drawing.Point(12, 333);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(379, 37);
+            this.btningresar.TabIndex = 12;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(379, 276);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(673, 382);
+            this.Controls.Add(this.btningresar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtrespon);
+            this.Controls.Add(this.txtcorreo);
+            this.Controls.Add(this.dtpnacimiento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mskboxcarnet);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Lucida Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox mskboxcarnet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpnacimiento;
+        private System.Windows.Forms.TextBox txtcorreo;
+        private System.Windows.Forms.TextBox txtrespon;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btningresar;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
